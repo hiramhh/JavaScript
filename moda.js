097b7eb02de4af3ea24c4f24c84fe8a7c2f258bd
Hiram Hernandez <hiramhdez98@gmail.com>
2022-07-01 18:03:44 -0500

    Calckulo de moda

--- moda.js
+++ moda.js
@@ -1,20 +1,34 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1,
-];
-
-const lista1Cont = {};
-lista1.map(
-    function(elemnto){
-        lista1Cont
+// const lista1 = [
+//     1,
+//     2,
+//     3,
+//     1,
+//     2,
+//     3,
+//     4,
+//     2,
+//     2,
+//     2,
+//     1,
+// ];
+function calculoDeModaAritmetica(lista){
+const listaCount = {};
+lista.map(
+    function(elemento){
+        if(listaCount[elemento]){
+            listaCount[elemento] += 1;
+        } else{
+            listaCount[elemento] = 1;
+        }
     }
-)
+);
+
+const listaArray = Object.entries(listaCount).sort(function (valorAcumulado, nuevoValor){
+    return valorAcumulado[1] - nuevoValor[1];
+}
+);
+
+const moda = listaArray[listaArray.length - 1];
+
+return moda;
+}

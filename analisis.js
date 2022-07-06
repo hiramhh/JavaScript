53e3ea770a8559710d3d40cb8706df9f8c2733fc
Hiram Hernandez <hiramhdez98@gmail.com>
2022-07-06 18:07:27 -0500

    Calculando el top 10 de salarios

--- analisis.js
+++ analisis.js
@@ -1,33 +1,15 @@
+// Helpers
+function esPar(numerito){
+    return (numerito % 2 === 0);
+}
+
 const salariosMx = mexico.map(
     function (personita) {
         return personita.salary;
     }
 );
 
-
-const salariosMxSorted = salariosMx.sort(
-    function (salaryA, salaryB) {
-        return salaryA - salaryB;
-    }
-);
-
-function esPar(numerito){
-    return (numerito % 2 === 0);
-}
-
-function calcularMediaAritmetica(lista){
-
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-
-    const promedioLista = sumaLista /lista.length;
-
-    return promedioLista;
-}
-
+//Calculadora de mediana 
 
 function medianaSalarios(lista){
     const mitad = parseInt(lista.length / 2);
@@ -45,5 +27,47 @@ function medianaSalarios(lista){
     }
 }
 
-console.log(
-    medianaSalarios(salariosMxSorted));
+
+//mediana General
+const salariosMxSorted = salariosMx.sort(
+    function (salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
+
+function calcularMediaAritmetica(lista){
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista /lista.length;
+
+    return promedioLista;
+}
+
+
+
+
+const medianaGeneralMx = medianaSalarios(salariosMxSorted);
+
+// Mediana del top 10%
+
+
+const spliceStart = (salariosMxSorted.length * 90) / 100;
+const spliceCount = salariosMxSorted.length - spliceStart;
+const salariosMxTop10 = salariosMxSorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10 = medianaSalarios(salariosMxTop10);
+
+
+console.log({
+    medianaGeneralMx,
+    medianaTop10,
+});

dc317593bca3258dac4eba5051f89b665d126721
Hiram Hernandez <hiramhdez98@gmail.com>
2022-07-06 16:40:04 -0500

    Calculando la mediana de salarios

---
+++ salarios.js
@@ -0,0 +1,98 @@
+const mexico = [];
+
+mexico.push({
+    name: "Raul",
+    salary: 600,
+});
+mexico.push({
+    name: "Ramces",
+    salary: 400,
+});
+mexico.push({
+    name: "Jocelyn",
+    salary: 420,
+});
+mexico.push({
+    name: "Juan",
+    salary: 430,
+});
+mexico.push({
+    name: "Eduardo",
+    salary: 800,
+});
+mexico.push({
+    name: "Alexis",
+    salary: 300,
+});
+mexico.push({
+    name: "Verenice",
+    salary: 600,
+});
+mexico.push({
+    name: "Samantha",
+    salary: 500,
+});
+mexico.push({
+    name: "Sabrina",
+    salary: 950,
+});
+mexico.push({
+    name: "josé",
+    salary: 210,
+});
+mexico.push({
+    name: "Alma",
+    salary: 1200,
+});
+mexico.push({
+    name: "Itzel",
+    salary: 800,
+});
+mexico.push({
+    name: "Fernanda",
+    salary: 450,
+});
+mexico.push({
+    name: "Luis",
+    salary: 980,
+});
+mexico.push({
+    name: "Daniel",
+    salary: 700,
+});
+mexico.push({
+    name: "",
+    salary: 600,
+});
+mexico.push({
+    name: "Paola",
+    salary: 700,
+});
+mexico.push({
+    name: "Eladio",
+    salary: 600,
+});
+mexico.push({
+    name: "Carlos",
+    salary: 580,
+});
+mexico.push({
+    name: "Eiza",
+    salary: 830,
+});
+mexico.push({
+    name: "Diana",
+    salary: 930,
+});
+mexico.push({
+    name: "Vicente",
+    salary: 820,
+});
+mexico.push({
+    name: "Maura",
+    salary: 600,
+});
+mexico.push({
+    name: "Xochilt",
+    salary: 780,
+});
